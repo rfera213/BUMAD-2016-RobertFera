a03b88d53443ba5997d03dd9dc5a13be4fa4921f
Robert Fera <rfera213@bu.edu>
2016-12-06 18:16:44 -0500

    Added beginnings of map module

---
+++ app/src/main/java/robertfera/mad/bu/edu/bumad_2016_robertfera/Map.java
@@ -0,0 +1,34 @@
+package robertfera.mad.bu.edu.bumad_2016_robertfera;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class Map extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // centers map on campus
+        mMap = googleMap;
+        LatLng campus = new LatLng(42.347, -71.095);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(campus, 13.25f));
+
+    }
+
+}
