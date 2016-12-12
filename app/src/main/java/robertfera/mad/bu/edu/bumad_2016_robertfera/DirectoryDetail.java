b9feb56fdf5197f0fa57e8d0fde6b364c2b5d114
Robert Fera <rfera213@bu.edu>
2016-12-12 16:56:55 -0500

    Tweaked DirectoryDetail visuals

--- app/src/main/java/robertfera/mad/bu/edu/bumad_2016_robertfera/DirectoryDetail.java
+++ app/src/main/java/robertfera/mad/bu/edu/bumad_2016_robertfera/DirectoryDetail.java
@@ -1,34 +1,33 @@
 package robertfera.mad.bu.edu.bumad_2016_robertfera;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Button;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class DirectoryDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // hide toolbar
         getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         getSupportActionBar().hide();
-
         setContentView(R.layout.activity_directory_detail);
-
-        TextView fullname = (TextView) findViewById(R.id.fullname);
-        TextView title = (TextView) findViewById(R.id.title);
-        TextView department = (TextView) findViewById(R.id.department);
-        Button phone = (Button) findViewById(R.id.phone);
-        Button email = (Button) findViewById(R.id.email);
-        Button address = (Button) findViewById(R.id.address);
 
         Bundle extras = getIntent().getExtras();
         String directoryFullname = extras.getString("fullname");
@@ -38,12 +37,23 @@ public class DirectoryDetail extends AppCompatActivity {
         String directoryEmail = extras.getString("email").toLowerCase();
         String directoryAddress = extras.getString("address").toLowerCase();
 
+        TextView fullname = (TextView) findViewById(R.id.fullname);
+        TextView title = (TextView) findViewById(R.id.title);
+        TextView department = (TextView) findViewById(R.id.department);
+
         fullname.setText(directoryFullname);
         title.setText(directoryTitle);
         department.setText(directoryDepartment);
-        phone.setText(directoryPhone);
-        email.setText(directoryEmail);
-        address.setText(directoryAddress);
+
+        ArrayList<StringPair> cells = new ArrayList<>();
+        if (directoryPhone != null && directoryPhone.length() > 0) cells.add(new StringPair("Phone", directoryPhone));
+        if (directoryEmail != null && directoryEmail.length() > 0) cells.add(new StringPair("Email", directoryEmail));
+        if (directoryAddress != null && directoryAddress.length() > 0) cells.add(new StringPair("Address", directoryAddress));
+
+        ListView buttons = (ListView)findViewById(R.id.buttons);
+        ListAdapter adapter = new DirectoryEntryListAdapter(DirectoryDetail.this, R.layout.detail_cell, cells);
+        buttons.setAdapter(adapter);
+
     }
 
     public void phone(View view) {
@@ -71,5 +81,30 @@ public class DirectoryDetail extends AppCompatActivity {
 
         // open maps module with search
 
+    }
+
+    public class DirectoryEntryListAdapter extends ArrayAdapter<StringPair> {
+
+        private Context context;
+        private ArrayList<StringPair> objects;
+
+        public DirectoryEntryListAdapter(Context context, int id, ArrayList<StringPair> objects) {
+            super(context, 0, objects);
+            this.objects = objects;
+            this.context = context;
+        }
+
+        public View getView(int position, View view, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.detail_cell, parent, false);
+
+            TextView label = (TextView) rowView.findViewById(R.id.label);
+            TextView info = (TextView) rowView.findViewById(R.id.info);
+
+            label.setText(objects.get(position).getKey());
+            info.setText(objects.get(position).getValue());
+
+            return rowView;
+        };
     }
 }
